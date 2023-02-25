df51c66d2019281779198ef154d0dc16bacd4748
Zorchenhimer <zorchenhimer@gmail.com>
2023-02-24 22:42:37 -0500

    Add a snake
    Adds a snake to the background layer, demonstrating redrawing the
    background animation one tile at a time.  D-pad changes the direction
    the snake is traveling.

--- main.asm
+++ main.asm
@@ -49,6 +49,16 @@ PPU_CTRL_NAMETABLE = %0000_0011
     sta $2000
 .endmacro
 
+; Button Constants
+BUTTON_A        = 1 << 7
+BUTTON_B        = 1 << 6
+BUTTON_SELECT   = 1 << 5
+BUTTON_START    = 1 << 4
+BUTTON_UP       = 1 << 3
+BUTTON_DOWN     = 1 << 2
+BUTTON_LEFT     = 1 << 1
+BUTTON_RIGHT    = 1 << 0
+
 .segment "VECTORS"
     .word NMI
     .word RESET
@@ -63,7 +73,21 @@ IdxB: .res 1
 IdxC: .res 1
 
 TmpX: .res 1
+TmpY: .res 1
 TmpZ: .res 1
+
+BlockUpdate: .res 1
+BlockDestroy: .res 2
+BlockNew: .res 2
+controller:     .res 1
+controller_Old: .res 1
+
+; Do a brick thing every time this hits zero
+FrameCount: .res 1
+
+BlockIdx: .res 1
+Blocks: .res 16 ; NT address for each block
+BlockDirection: .res 1
 
 ; Every page starts with its page number in the
 ; first byte. This is so we can return to
@@ -122,6 +146,60 @@ NMI:
     sta z:waves_currentFrame
 :
 @noAnim:
+
+    lda BlockUpdate
+    beq @NoBlockUpdate
+    lda #0
+    sta BlockUpdate
+
+    ; Calculate the NT address for the X/Y coord
+    clc
+    ldx BlockNew+1 ; Y
+    lda BlockNew+0 ; X
+    adc NTLow, x
+    tay
+    lda #0
+    adc NTHigh, x
+
+    bit $2002
+    sta $2006
+    sty $2006
+
+    ; write block tile
+    lda #0
+    sta $2007
+
+    ; Remove the oldest block
+    clc
+    ldx BlockDestroy+1 ; Y
+    lda BlockDestroy+0 ; X
+    adc NTLow, x
+    tay
+    lda #0
+    adc NTHigh, x
+    sta $2006
+    sty $2006
+
+    ; find BG tile ID
+    lda BlockDestroy+1
+    and #7
+    tax
+    lda Index_BgAnimRows, x
+    sta TmpY
+
+    lda BlockDestroy+0
+    and #7
+    clc
+    adc TmpY
+    sta $2007
+
+@NoBlockUpdate:
+    lda #0
+    sta BlockDestroy+0
+    sta BlockDestroy+1
+    sta BlockNew+0
+    sta BlockNew+1
+
     lda z:waves_AnimOdd
     jsr waves_WriteCachedRow
 
@@ -208,6 +286,28 @@ RESET:
     cpx #4
     bne :-
 
+    ; Load the block tile
+    lda #0
+    sta $2006
+    sta $2006
+    lda #$FF
+    ldx #16
+:
+    sta $2007
+    dex
+    bne :-
+
+    lda #$10
+    sta $2006
+    lda #$00
+    sta $2006
+    lda #$FF
+    ldx #16
+:
+    sta $2007
+    dex
+    bne :-
+
     lda #$88
     sta PpuControl
     sta $2000
@@ -223,10 +323,209 @@ RESET:
     lda #0
     sta z:waves_AnimOdd
 
+    lda #BUTTON_DOWN
+    sta BlockDirection
+
 ; Main frame loop
-:   jsr waves_CacheRow
+FrameLoop:
+    dec FrameCount
+    bne :+
+
+    lda #5
+    sta FrameCount
+    jsr BlockThing
+:
+
+    jsr waves_CacheRow
     jsr WaitForNMI
-    jmp :-
+    jmp FrameLoop
+
+; Animate/move Blocks
+BlockThing:
+    jsr ReadController
+    inc BlockUpdate
+
+    ldx BlockDirection
+    lda #BUTTON_LEFT
+    and controller
+    beq :+
+    ldx #1
+    jmp @draw
+:
+    lda #BUTTON_RIGHT
+    and controller
+    beq :+
+    ldx #0
+    jmp @draw
+:
+    lda #BUTTON_UP
+    and controller
+    beq :+
+    ldx #3
+    jmp @draw
+:
+    lda #BUTTON_DOWN
+    and controller
+    beq :+
+    ldx #2
+:
+
+@draw:
+    stx BlockDirection
+
+    ;; Prep Draw new block
+    lda BlockIdx
+    asl a
+    tax
+
+    ; grab last block's x/y
+    lda Blocks+0, x
+    sta BlockNew+0
+    lda Blocks+1, x
+    sta BlockNew+1
+
+    ldy BlockDirection
+    bne :+
+    ; Right
+    inc BlockNew+0
+    jmp @mathdone
+:
+    cpy #1
+    bne :+
+    ; Left
+    dec BlockNew+0
+    jmp @mathdone
+:
+    cpy #3
+    bne :+
+    ; Up
+    dec BlockNew+1
+    jmp @mathdone
+:
+    ; down
+    inc BlockNew+1
+
+@mathdone:
+    ; Handle X wrap
+    lda BlockNew+0
+    cmp #32
+    bne :+
+    lda #0
+    sta BlockNew+0
+:
+    cmp #$FF
+    bne :+
+    lda #31
+    sta BlockNew+0
+:
+
+    ; Handle Y wrap
+    lda BlockNew+1
+    cmp #30
+    bne :+
+    lda #0
+    sta BlockNew+1
+:
+    cmp #$FF
+    bne :+
+    lda #29
+    sta BlockNew+1
+:
+
+    ; remove oldest block
+    inc BlockIdx
+    lda BlockIdx
+    cmp #8
+    bne :+
+    lda #0
+    sta BlockIdx
+:
+    asl a
+    tax
+    lda Blocks+0, x
+    sta BlockDestroy+0
+    lda Blocks+1, x
+    sta BlockDestroy+1
+
+    ; save new block's coords in place of the old block
+    lda BlockNew+0
+    sta Blocks+0, x
+    lda BlockNew+1
+    sta Blocks+1, x
+    rts
+
+;    ; find new block x,y
+;    tay
+;    clc
+;    lda Blocks+0, y
+;    adc BlockMoveX, x
+;    sta TmpY
+;
+;    clc
+;    lda Blocks+1, y
+;    adc BlockMoveY, x
+;    sta TmpX
+;
+;    ; find the oldest (next) block
+;    inc BlockIdx
+;    lda BlockIdx
+;    cmp #8
+;    bne :+
+;    ; handle wrap around
+;    lda #0
+;    sta BlockIdx
+;:
+;
+;    ; remove oldest block (next in the queue)
+;    asl a
+;    tay
+;    ldx Blocks+0, y
+;    lda NTHigh, x
+;    sta BlockDestroy+1
+;    lda NTLow, x
+;    sta BlockDestroy+0
+;
+;    clc
+;    lda Blocks+1, y
+;    adc BlockDestroy+0
+;    sta BlockDestroy+0
+;    lda #0
+;    adc BlockDestroy+1
+;    sta BlockDestroy+1
+;
+;    ; New block
+;    ldx TmpY
+;    lda NTHigh, x
+;    sta BlockNew+1
+;    lda NTLow, x
+;    sta BlockNew+0
+;
+;    clc
+;    lda TmpX
+;    adc 
+;
+;
+;    rts
+
+; Nametable row address starts (Y values)
+NTHigh:
+.repeat 32, i
+    .byte .hibyte($2000 + (i * 32))
+.endrepeat
+
+NTLow:
+.repeat 32, i
+    .byte .lobyte($2000 + (i * 32))
+.endrepeat
+
+; These movement 
+BlockMoveX:
+    ;     R    L  D  U
+    .byte 1, $FF, 0, 0
+
+BlockMoveY:
+    ;     R  L   D    U
+    .byte 0, 0, 32, $E0
 
 WaitForNMI:
 :   bit Sleeping
@@ -287,4 +586,30 @@ MMC1_Select_Page:
     sta $E000
     rts
 
+ReadController:
+    lda controller
+    sta controller_Old
+
+    ; Freeze input
+    lda #1
+    sta $4016
+    lda #0
+    sta $4016
+
+    ldx #$08
+:
+    lda $4016
+    lsr A           ; Bit0 -> Carry
+    rol controller ; Bit0 <- Carry
+    dex
+    bne :-
+    rts
+
 .include "bg_anim.asm"
+
+; Tile ID's of the start of each row of tiles in
+; the background animation.
+Index_BgAnimRows:
+    .repeat 8, i
+    .byte (i * 8) + $C0
+    .endrepeat
